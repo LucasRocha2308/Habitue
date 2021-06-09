d5b84cdbfe88588958eeda79e96349e0e629bba1
Lucas <lucas_rocha_25@hotmail.com>
2021-06-09 11:53:08 -0300

    feature:estrutura basica do projeto

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { Switch, Route } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Switch>
+      {/* <Route path="/" exact component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/habit" component={Habit} />
+      <Route path="/register" component={Register} />
+      <Route path="/dashboard" component={Dashboard} isPrivate /> */}
+    </Switch>
+  );
+};
+
+export default Routes;
