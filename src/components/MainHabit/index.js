a3153371601f0b09e1b757164574fc97ea0f23e7
Daniel Epichin <d.pena@hotmail.com>
2021-06-14 09:55:43 -0300

    feat: tela hábito feita sem imagem e segunda tela.

--- src/components/MainHabit/index.js
+++ src/components/MainHabit/index.js
@@ -1,3 +1,20 @@
-const MainHabit = () => {};
+import { ButtonWhite } from "../Button";
+import { BackCard } from "./style";
+
+const MainHabit = () => {
+  return (
+    <>
+      <BackCard>
+        <p>
+          Ainda não cadastrou seu hábito, cadastre clicando no botão abaixo.
+        </p>
+        <ButtonWhite>
+          Cadastrar <br />
+          Hábitos
+        </ButtonWhite>
+      </BackCard>
+    </>
+  );
+};
 
 export default MainHabit;
