4b68f8b8963a6e5b34bca30b39389c32d2ef6430
Daniel Epichin <d.pena@hotmail.com>
2021-06-15 15:10:10 -0300

    feat: imagem de fundo e label colocada no card de hábitos.

--- src/components/CardHabit/index.js
+++ src/components/CardHabit/index.js
@@ -18,9 +18,13 @@ const CardHabit = () => {
           <CardStyle key={elem.id}>
             <BntRemove onClick={() => removeHabit(elem.id)} />
             <h2>{elem.title}</h2>
+            <label>Categoria</label>
             <p>{elem.category}</p>
+            <label>Dificuldade</label>
             <p>{elem.difficulty}</p>
+            <label>Frequencia do hábito</label>
             <p>{elem.frequency}</p>
+            <label>Quantos pontos vale</label>
             <p>{elem.how_much_achieved}</p>
           </CardStyle>
         );
